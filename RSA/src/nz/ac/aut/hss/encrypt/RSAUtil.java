0a053cfb93e58a7db12ccdf936642a73a5024bbe
Meash <m4rtinsch@gmail.com>
2014-08-13 17:02:42 +1200

    Added RSA Util class header.

---
+++ RSA/src/nz/ac/aut/hss/encrypt/RSAUtil.java
@@ -0,0 +1,31 @@
+package nz.ac.aut.hss.encrypt;
+
+import java.math.BigInteger;
+
+/**
+ * @author Martin Schrimpf
+ * @created 13.08.2014
+ */
+public class RSAUtil {
+	public static boolean probablyPrime(BigInteger n) {
+		return true; // TODO
+	}
+
+	public static EudlidResult extendedEuclid(BigInteger m, BigInteger n) {
+		return null; // TODO
+	}
+
+	private static class EudlidResult {
+		public final BigInteger d, s, t;
+
+		public EudlidResult(BigInteger d, BigInteger s, BigInteger t) {
+			this.d = d;
+			this.s = s;
+			this.t = t;
+		}
+	}
+
+	public static BigInteger modularExponentiation(BigInteger a, BigInteger b, BigInteger n) {
+		return null; // TODO
+	}
+}
