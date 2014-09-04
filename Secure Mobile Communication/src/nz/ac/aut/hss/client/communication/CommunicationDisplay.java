098eaaf708bf4a4ac434d2e23a1a91065e303b8f
Meash <m4rtinsch@gmail.com>
2014-09-05 10:02:40 +1200

    Added confidential and authentic flags to message display.

--- Secure Mobile Communication/src/nz/ac/aut/hss/client/communication/CommunicationDisplay.java
+++ Secure Mobile Communication/src/nz/ac/aut/hss/client/communication/CommunicationDisplay.java
@@ -5,5 +5,5 @@ package nz.ac.aut.hss.client.communication;
  * @created 04.09.2014
  */
 public interface CommunicationDisplay {
-	public void addReceivedMessage(final String text);
+	public void addReceivedMessage(final String text, final boolean confidential, final boolean authentic);
 }
